c4e4e393dd7b4a138fee516c87b695190edce8f9
Aveev Bair <aveevb123@gmail.com>
2023-01-13 18:13:31 +0300

    'fix: fix bug with last checkbox'

--- src/components/ListBody.jsx
+++ src/components/ListBody.jsx
@@ -4,15 +4,18 @@ import DeleteTaskBtn from "./DeleteTaskBtn";
 
 const LOCAL_STORAGE_KEY = 'todoArr';
 
+const setLocalStorage = (value) => localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(value));
+const makeCopy = (curr) => JSON.parse(JSON.stringify(curr));
+
 const ListBody = () => {
     const [text, setText] = useState('');
     const [taskObj, setTaskObj] = useState(JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || {});
-    const setLocalStorage = (value) => localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(value));
     const createTask = () => {
             if (text.trim() === '') return taskObj
             setTaskObj(curr => {
                 const idTask = +new Date();
-                const newTaskObj = {...curr, [idTask]: {text, completed: false, id: idTask}};
+                const newTaskObj = makeCopy(curr);
+                newTaskObj[idTask] = {text, completed: false, id: idTask};
                 setLocalStorage(newTaskObj);
                 return newTaskObj
             });
@@ -24,15 +27,23 @@ const ListBody = () => {
     }
     const clearAll = () => {
         localStorage.removeItem(LOCAL_STORAGE_KEY);
-        setTaskObj([]);
+        setTaskObj({});
     };
     const handleDelete = (id) => {
         setTaskObj(curr => {
-            const newObj = {...curr};
+            const newObj = makeCopy(curr);
             delete newObj[id];
             setLocalStorage(newObj);
             return newObj
         });
+    }
+    const handleToggleCheck = (key) => {
+        setTaskObj((curr) => {
+            const newTaskObj = makeCopy(curr);
+            newTaskObj[key].completed = !(newTaskObj[key].completed);
+            setLocalStorage(newTaskObj);
+            return newTaskObj
+        })
     }
 
     return (
@@ -42,6 +53,7 @@ const ListBody = () => {
                 <ul>
                     {Object.values(taskObj).map(item =>
                         <li key={item.id}>
+                            <input type="checkbox" defaultChecked={item.completed} onClick={() => handleToggleCheck(item.id)}/>
                             <input onBlur={(e) => handleChange(e, item.id)} defaultValue={item.text}/>
                             <DeleteTaskBtn onClick={() => handleDelete(item.id)}/>
                         </li>
